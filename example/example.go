18f5674a356d445bb1900123859b2db805e13b9f
Ddo <joeddo89@gmail.com>
2015-01-20 01:55:05 +0700

    update example

--- example/example.go
+++ example/example.go
@@ -3,5 +3,29 @@ package main
 import "github.com/ddo/go-crawler"
 
 func main() {
+	no := 0
 
+	c := crawler.New(50)
+
+	ch_url := make(chan string)
+	ch_err := make(chan error)
+	ch_done := make(chan bool)
+
+	go c.Start("http://facebook.com", ch_url, ch_err, ch_done)
+
+loop:
+	for {
+		select {
+		case url := <-ch_url:
+			no++
+			println(no, "\t ", url)
+		case err := <-ch_err:
+			println("error ", err)
+		case <-ch_done:
+			println("done thanks god")
+			break loop
+		}
+	}
+
+	println("done thanks god again")
 }
