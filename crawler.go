82b9d0f83756ad1b7034ee7bec4d3a45e2cf4757
Ddo <joeddo89@gmail.com>
2015-01-20 16:35:16 +0700

    refactor, hide channels

--- crawler.go
+++ crawler.go
@@ -5,20 +5,17 @@ import (
 )
 
 type Crawler struct {
-	limit int
+	u       string
+	limit   int
+	ch_url  chan string
+	ch_err  chan error
+	ch_done chan bool
+	picker  Picker
+	filters []Filter
 }
 
-func New(limit int) *Crawler {
-	return &Crawler{
-		limit: limit,
-	}
-}
-
-func (c *Crawler) Start(u string, ch_url chan string, ch_err chan error, ch_done chan bool) {
+func New(u string, limit int) *Crawler {
 	u_obj, _ := url.Parse(u)
-
-	//anchor picker
-	picker_anchor := &AnchorPicker{}
 
 	//unique filter
 	filter_url := &UrlFilter{}
@@ -33,21 +30,49 @@ func (c *Crawler) Start(u string, ch_url chan string, ch_err chan error, ch_done
 		u_obj,
 	}
 
-	c.crawl(u, []Picker{picker_anchor}, []Filter{filter_url, filter_unique, filter_samehost}, ch_url, ch_err, ch_done)
+	return &Crawler{
+		u:       u,
+		limit:   limit,
+		ch_url:  make(chan string),
+		ch_err:  make(chan error),
+		ch_done: make(chan bool),
+		picker:  &AnchorPicker{},
+		filters: []Filter{filter_url, filter_unique, filter_samehost},
+	}
 }
 
-func (c *Crawler) crawl(u string, pickers []Picker, filters []Filter, ch_url chan string, ch_err chan error, ch_done chan bool) {
+type receiver_url func(string)
+type receiver_err func(error)
 
-	urls, err := Fetch(u, pickers[0]) //hardcode
+func (c *Crawler) Start(r_url receiver_url, r_err receiver_err) {
+	go c.crawl(c.u)
+
+loop:
+	for {
+		select {
+		case url := <-c.ch_url:
+			r_url(url)
+		case err := <-c.ch_err:
+			r_err(err)
+		case <-c.ch_done:
+			break loop
+		}
+	}
+
+	return
+}
+
+func (c *Crawler) crawl(u string) {
+	urls, err := Fetch(u, c.picker)
 
 	if err != nil {
-		ch_err <- err
+		c.ch_err <- err
 		return
 	}
 
 parent_loop:
 	for _, child_u_obj := range urls {
-		for _, f := range filters {
+		for _, f := range c.filters {
 			if !f.Filter(child_u_obj) {
 				continue parent_loop
 			}
@@ -57,20 +82,20 @@ parent_loop:
 		c.limit--
 
 		select {
-		case <-ch_done:
+		case <-c.ch_done:
 			return
 		default:
 		}
 
 		if c.limit < 0 {
-			close(ch_done)
+			close(c.ch_done)
 			return
 		}
 
 		child_url := child_u_obj.String()
 
-		ch_url <- child_url
+		c.ch_url <- child_url
 
-		go c.crawl(child_url, pickers, filters, ch_url, ch_err, ch_done)
+		go c.crawl(child_url)
 	}
 }
--- example/example.go
+++ example/example.go
@@ -1,31 +1,21 @@
 package main
 
-import "github.com/ddo/go-crawler"
+import (
+	"fmt"
+	"github.com/ddo/go-crawler"
+)
 
 func main() {
 	no := 0
 
-	c := crawler.New(50)
+	c := crawler.New("http://talktv.vn/", 50)
 
-	ch_url := make(chan string)
-	ch_err := make(chan error)
-	ch_done := make(chan bool)
+	c.Start(func(url string) {
+		no++
+		fmt.Println(no, "\t ", url)
+	}, func(err error) {
+		fmt.Println("error\t", err)
+	})
 
-	go c.Start("http://facebook.com", ch_url, ch_err, ch_done)
-
-loop:
-	for {
-		select {
-		case url := <-ch_url:
-			no++
-			println(no, "\t ", url)
-		case err := <-ch_err:
-			println("error ", err)
-		case <-ch_done:
-			println("done thanks god")
-			break loop
-		}
-	}
-
-	println("done thanks god again")
+	fmt.Println("done thanks god")
 }
