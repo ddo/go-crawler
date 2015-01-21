bed5def6990a1905a58a13c33f62a7cbf3afadd9
Ddo <joeddo89@gmail.com>
2015-01-21 17:06:16 +0700

    expose Filters properties

--- filter.go
+++ filter.go
@@ -17,11 +17,11 @@ func (f *UrlFilter) Filter(u *url.URL) bool {
 
 //no duplicated Filter
 type UniqueFilter struct {
-	urls []*url.URL
+	Us []*url.URL
 }
 
 func (f *UniqueFilter) Filter(u *url.URL) bool {
-	for _, old_u := range f.urls {
+	for _, old_u := range f.Us {
 		if *u == *old_u {
 			return false
 		}
@@ -37,16 +37,16 @@ func (f *UniqueFilter) Filter(u *url.URL) bool {
 		}
 	}
 
-	f.urls = append(f.urls, u)
+	f.Us = append(f.Us, u)
 
 	return true
 }
 
 //same hostname only
 type SameHostFilter struct {
-	root_url *url.URL
+	U *url.URL
 }
 
 func (f *SameHostFilter) Filter(u *url.URL) bool {
-	return f.root_url.Host == u.Host
+	return f.U.Host == u.Host
 }
